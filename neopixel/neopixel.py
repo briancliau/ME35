0309c6bb601b23f718b51eb65e123ce49a22b346
Brian Liau <brian.liau@tufts.edu>
2026-09-09 11:30:41 -0400

    Added neopixel python code

---
+++ neopixel/neopixel.py
@@ -0,0 +1,39 @@
+from machine import Pin
+import neopixel
+import time
+
+lights = neopixel.NeoPixel(Pin(15), 4)
+
+x = 0
+y = 0
+z = 0
+
+for i in range(350):
+    if 0 < x < 50:
+        x += 1
+    else:
+        x = 0
+
+    if 0 < y < 50:
+        y -= 1
+    else:
+        y = 50
+
+    z = 0
+
+    if i % 2 == 0:
+        if i % 6 == 2:
+            lights[0] = (x, y, z)
+            lights[1] = (y, z, x)
+        if i % 6 == 4:
+            lights[0] = (y, x, z)
+            lights[1] = (x, z, y)
+        else:
+            lights[0] = (y, z, x)
+            lights[1] = (z, y, x)
+    else:
+        lights[0] = (0, 0, 0)
+        lights[1] = (0, 0, 0)
+
+    lights.write()  # Send pixel changes to hardware on every iteration
+    time.sleep_ms(1000)
